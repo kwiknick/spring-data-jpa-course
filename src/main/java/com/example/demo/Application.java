21acdc0571df662bed38fbee4fce109cea2b0219
Nicholas Willard <nswillard@hotmail.com>
2023-06-04 13:33:41 -0500

    Added code to perform CRUD operations on some made up students

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class Application {
 
@@ -19,9 +21,43 @@ public class Application {
                     "Maria",
                     "Jones",
                     "maria.jones@willard.io",
+                    41
+            );
+
+            StudentEntity nick = new StudentEntity(
+                    "Nick",
+                    "Willard",
+                    "nick.willard@willard.io",
                     40
             );
-            studentRepository.save(maria);
+
+            System.out.println("Adding maria and nick to the Database");
+            studentRepository.saveAll(List.of(maria, nick));
+
+            System.out.print("Number of Students: ");
+            System.out.println(studentRepository.count());
+
+            studentRepository
+                    .findById(2L)
+                    .ifPresentOrElse(
+                            System.out::println,
+                            () -> System.out.println("Student with ID 2 not found"));
+
+            studentRepository
+                    .findById(3L)
+                    .ifPresentOrElse(
+                            System.out::println,
+                            () -> System.out.println("Student with ID 3 not found"));
+
+            System.out.println("Select all Students");
+            List<StudentEntity> students = studentRepository.findAll();
+            students.forEach(System.out::println);
+
+            System.out.println("Delete maria");
+            studentRepository.deleteById(1L);
+
+            System.out.print("Number of Students: ");
+            System.out.println(studentRepository.count());
         };
     }
 }
