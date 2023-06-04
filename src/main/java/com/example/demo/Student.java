7b651455e23d11298a44311d275fae91c1e08491
Nicholas Willard <nswillard@hotmail.com>
2023-06-04 08:35:24 -0500

    Added sequence and lowered Java version to 16

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -6,15 +6,27 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
 @Getter
 @Setter
 @ToString
 @AllArgsConstructor
-@Entity
+@Entity(name = "Student")
 public class Student {
     @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String firstName;
     private String lastName;
