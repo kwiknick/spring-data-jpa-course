566a2fccd5cd7632605bd80b6f968a043680482c
Nicholas Willard <nswillard@hotmail.com>
2023-06-04 21:48:28 -0500

    Setup the relationship between the Student and IdCard tables

--- src/main/java/com/example/demo/StudentIdCardEntity.java
+++ src/main/java/com/example/demo/StudentIdCardEntity.java
@@ -5,11 +5,14 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
@@ -52,6 +55,13 @@ public class StudentIdCardEntity {
             length = 15
     )
     private String cardNumber;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(
+            name = "student_id",
+            referencedColumnName = "id"
+    )
+    private StudentEntity student;
 
     public StudentIdCardEntity(String cardNumber) {
         this.cardNumber = cardNumber;
